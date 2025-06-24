3fc8147cf96d5dfeff2a37620bcaad25d2ef3e6b
Lubodi-Code <luisfelipebolanosdixon@gmail.com>
2025-06-23 23:26:45 -0600

    segundo comit

--- src/main/java/com/demo/listeners/MobSpawnListener.java
+++ src/main/java/com/demo/listeners/MobSpawnListener.java
@@ -2,9 +2,9 @@ package com.demo.listeners;
 
 import com.demo.goals.BreakBlockGoal;
 import com.demo.managers.ConfigManager;
-import net.minecraft.world.entity.EntityCreature;
+import net.minecraft.world.entity.PathfinderMob; // Updated to PathfinderMob
 import org.bukkit.Bukkit;
-import org.bukkit.craftbukkit.v1_20_R3.entity.CraftLivingEntity;
+import org.bukkit.craftbukkit.v1_21_R1.entity.CraftLivingEntity; // Updated version
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Monster;
@@ -29,7 +29,9 @@ public class MobSpawnListener implements Listener {
             return;
         }
         try {
-            EntityCreature nms = ((CraftLivingEntity) entity).getHandle();
+            CraftLivingEntity craftEntity = (CraftLivingEntity) entity;
+            // Use PathfinderMob instead of EntityCreature for 1.21.6
+            PathfinderMob nms = (PathfinderMob) craftEntity.getHandle();
             nms.goalSelector.addGoal(3, new BreakBlockGoal(nms, config));
             Bukkit.getLogger().info("Added break block goal to: " + entity.getType());
         } catch (Exception e) {
